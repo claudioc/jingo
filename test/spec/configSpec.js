112697eb555eed9a4ada37c0c57fbf2c6173c6fe
Claudio Cicali <claudio.cicali@gmail.com>
2013-10-23 23:16:24 +0200

    Refactoring of the config

---
+++ test/spec/configSpec.js
@@ -0,0 +1,74 @@
+var chai   = require("chai");
+var expect = chai.expect;
+var yaml   = require('yaml-js');
+
+var configKeys = ['application', 'features', 'server', 'authorization', 'authentication'];
+
+var Config = require("../../lib/config");
+
+describe ("Config", function() {
+
+  beforeEach(function() {
+    Config.setup();
+  });  
+
+  it ("should render a valid config in YAML", function() {
+
+    var def = yaml.load(Config.sample());
+
+    expect(Object.keys(def).join('')).to.equal(configKeys.join(''));
+
+    expect(def.application.title).to.equal('Jingo');
+    expect(def.application.repository).to.equal('/absolute/path/to/your/repo');
+    expect(def.application.docSubdir).to.equal('');
+    expect(def.application.remote).to.equal('');
+    expect(def.application.pushInterval).to.equal(30);
+    expect(def.application.secret).to.equal('change me');
+
+    expect(def.features.codemirror).to.be.true;
+    expect(def.features.markitup).to.be.false;
+
+    expect(def.server.hostname).to.equal('localhost');
+    expect(def.server.port).to.equal(6067);
+    expect(def.server.localOnly).to.be.false;
+    expect(def.server.baseUrl).to.equal("http://localhost:6067");
+
+    expect(def.authorization.anonRead).to.be.true;
+    expect(def.authorization.validMatches).to.equal('.+');
+
+    expect(def.authentication.google.enabled).to.be.true;
+    expect(def.authentication.alone.enabled).to.be.false;
+    expect(def.authentication.alone.username).to.equal('');
+    expect(def.authentication.alone.passwordHash).to.equal('');
+    expect(def.authentication.alone.email).to.equal('');
+  });
+
+  it ("should get the config as a whole", function() {
+
+    var c = Config.get();
+
+    expect(c).to.be.an("undefined");
+
+    Config.setup({
+      test: 23
+    });
+
+    expect(Config.get()).to.be.an("object");
+  });
+
+  it ("should get a variable from the config", function() {
+
+    Config.setup({
+      test: 23,
+      test1: {
+        test2: 44
+      }
+    });
+
+    expect(Config.get("test")).to.equal(23);
+    expect(Config.get("test1.test2")).to.equal(44);
+    expect(Config.get("test1.test3")).to.be.an("undefined");
+    expect(Config.get("test1.test3", "pop-art")).to.equal("pop-art");
+  });
+
+});
