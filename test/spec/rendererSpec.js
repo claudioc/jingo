cd3780aca0a93cd071d03e8b7ff659e58e745724
Maxime Lathuilière <k@maxlath.eu>
2017-01-08 14:57:54 +0100

    fixed renderer for bold and italic bracket tags (#189)

--- test/spec/rendererSpec.js
+++ test/spec/rendererSpec.js
@@ -44,6 +44,21 @@ describe('Renderer', function () {
     expect(Renderer.render(text)).to.be.equal('<p><a class="internal" href="/wiki/Foo">Foo</a>, <a class="internal" href="/wiki/Bar">Bar</a></p>\n')
   })
 
+  it('should render bold bracket tags', function () {
+    var text = 'foo **[[Bar]]** buzz'
+    expect(Renderer.render(text)).to.be.equal('<p>foo <strong><a class="internal" href="/wiki/Bar">Bar</a></strong> buzz</p>\n')
+  })
+
+  it('should render italic bracket tags', function () {
+    var text = 'foo *[[Bar]]* buzz'
+    expect(Renderer.render(text)).to.be.equal('<p>foo <em><a class="internal" href="/wiki/Bar">Bar</a></em> buzz</p>\n')
+  })
+
+  it('should render italic bold bracket tags', function () {
+    var text = 'foo ***[[Bar]]*** buzz'
+    expect(Renderer.render(text)).to.be.equal('<p>foo <strong><em><a class="internal" href="/wiki/Bar">Bar</a></em></strong> buzz</p>\n')
+  })
+
   it('should replace {{TOC}} with the table of contents', function () {
     var text = '{{TOC}}\n\n # Heading 1 \n\n This is some text'
     expect(Renderer.render(text)).to.be.equal('<ul>\n<li><p><a href="#heading-1">Heading 1</a></p>\n<h1 id="heading-1">Heading 1</h1>\n<p>This is some text</p>\n</li>\n</ul>\n')
